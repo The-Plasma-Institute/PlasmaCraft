7a9617fdb693d277cfd34799ed5b04cb8569b6c3
JoelLucaAdams <joelucadams@gmail.com>
2024-02-06 14:54:35 +0000

    Added mineable sapphire blocks and sapphire ore variants

--- src/main/java/net/joelinrome/realisticfusionreactors/block/ModBlocks.java
+++ src/main/java/net/joelinrome/realisticfusionreactors/block/ModBlocks.java
@@ -2,10 +2,12 @@ package net.joelinrome.realisticfusionreactors.block;
 
 import net.joelinrome.realisticfusionreactors.RealisticFusionReactors;
 import net.joelinrome.realisticfusionreactors.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,6 +22,11 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
     public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE).strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+    public static final RegistryObject<Block> DEEPSLATE_SAPPHIRE_ORE = registerBlock("deepslate_sapphire_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).strength(3f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+    public static final RegistryObject<Block> NETHER_SAPPHIRE_ORE = registerBlock("nether_sapphire_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHERRACK).strength(1f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+    public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/net/joelinrome/realisticfusionreactors/item/ModCreativeModTabs.java
+++ src/main/java/net/joelinrome/realisticfusionreactors/item/ModCreativeModTabs.java
@@ -18,8 +18,15 @@ public class ModCreativeModTabs {
             .displayItems(((itemDisplayParameters, output) -> {
                 output.accept(ModItems.RAW_SAPPHIRE.get());
                 output.accept(ModItems.SAPPHIRE.get());
+
                 output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
                 output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+
+                output.accept(ModBlocks.SAPPHIRE_ORE.get());
+                output.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                output.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                output.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
             }))
             .build());
     public static void register(IEventBus eventBus) {

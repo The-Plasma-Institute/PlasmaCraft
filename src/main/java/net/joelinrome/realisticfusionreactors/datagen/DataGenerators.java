9baf8c0aa2581c24fb39a1dcc6c973c3effed0a5
JoelLucaAdams <joelucadams@gmail.com>
2024-02-16 14:58:07 +0000

    Added sapphire ore generation underground

--- src/main/java/net/joelinrome/realisticfusionreactors/datagen/DataGenerators.java
+++ src/main/java/net/joelinrome/realisticfusionreactors/datagen/DataGenerators.java
@@ -26,6 +26,8 @@ public class DataGenerators {
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
 
+        generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
+
         ModBlockTagGenerator blockTagGenerator = generator.addProvider(event.includeServer(),
                 new ModBlockTagGenerator(packOutput, lookupProvider, existingFileHelper));
         generator.addProvider(event.includeServer(), new ModItemTagGenerator(packOutput, lookupProvider, blockTagGenerator.contentsGetter(), existingFileHelper));

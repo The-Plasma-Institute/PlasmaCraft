ebd2388a43d152680a118856b15adf4c03a410e9
JoelLucaAdams <joelucadams@gmail.com>
2024-02-26 17:16:44 +0000

    Fixed JEI animated arrows

--- src/main/java/net/joelinrome/plasmacraft/compat/DeuteriumExtractorCategory.java
+++ src/main/java/net/joelinrome/plasmacraft/compat/DeuteriumExtractorCategory.java
@@ -3,6 +3,8 @@ package net.joelinrome.plasmacraft.compat;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
@@ -11,6 +13,8 @@ import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.joelinrome.plasmacraft.PlasmaCraft;
 import net.joelinrome.plasmacraft.block.ModBlocks;
 import net.joelinrome.plasmacraft.repice.DeuteriumExtractorRecipe;
+import net.joelinrome.plasmacraft.repice.GemPolishingRecipe;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -25,10 +29,13 @@ public class DeuteriumExtractorCategory implements IRecipeCategory<DeuteriumExtr
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final IDrawableAnimated animatedArrow;
 
     public DeuteriumExtractorCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 80);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.DEUTERIUM_EXTRACTOR_BLOCK.get()));
+        this.animatedArrow = helper.drawableBuilder(TEXTURE, 176, 0, 14, 26)
+                .buildAnimated(200, IDrawableAnimated.StartDirection.TOP, true);
     }
 
     @Override
@@ -56,5 +63,11 @@ public class DeuteriumExtractorCategory implements IRecipeCategory<DeuteriumExtr
         iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.INPUT, 80, 11).addIngredients(deuteriumExtractorRecipe.getIngredients().get(0));
 
         iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.OUTPUT, 80, 59).addItemStack(deuteriumExtractorRecipe.getResultItem(null));
+    }
+
+    @Override
+    public void draw(DeuteriumExtractorRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        IRecipeCategory.super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
+        animatedArrow.draw(guiGraphics, 85, 30);
     }
 }
--- src/main/java/net/joelinrome/plasmacraft/compat/GemPolishingCategory.java
+++ src/main/java/net/joelinrome/plasmacraft/compat/GemPolishingCategory.java
@@ -4,6 +4,7 @@ import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
@@ -12,6 +13,7 @@ import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.joelinrome.plasmacraft.PlasmaCraft;
 import net.joelinrome.plasmacraft.block.ModBlocks;
 import net.joelinrome.plasmacraft.repice.GemPolishingRecipe;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -24,13 +26,13 @@ public class GemPolishingCategory implements IRecipeCategory<GemPolishingRecipe>
 
     private final IDrawable background;
     private final IDrawable icon;
-    private final IDrawableAnimated animatedArrow; // This doesn't work yet :(
+    private final IDrawableAnimated animatedArrow;
 
     public GemPolishingCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 0, 0,176, 80); // Draws just the top half of the GUI texture
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.GEM_POLISHING_STATION.get())); // Icon displayed in JEI
         this.animatedArrow = helper.drawableBuilder(TEXTURE, 176, 0, 14, 26)
-                .buildAnimated(200, IDrawableAnimated.StartDirection.TOP, true);
+                .buildAnimated(200, IDrawableAnimated.StartDirection.TOP, true); // Adds an animated arrow
     }
 
     @Override
@@ -58,5 +60,11 @@ public class GemPolishingCategory implements IRecipeCategory<GemPolishingRecipe>
         // i and i1 are the locations of the input slot
         iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.INPUT, 80, 11).addIngredients(recipe.getIngredients().get(0));
         iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.OUTPUT, 80, 59).addItemStack(recipe.getResultItem(null));
+    }
+
+    @Override
+    public void draw(GemPolishingRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        IRecipeCategory.super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
+        animatedArrow.draw(guiGraphics, 85, 30);
     }
 }

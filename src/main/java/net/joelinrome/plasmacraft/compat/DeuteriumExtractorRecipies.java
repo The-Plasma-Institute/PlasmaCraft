6410ac444b82e48f5a6e9a52465079cad8e96b1d
ShaunD137 <wzx509@york.ac.uk>
2024-02-26 12:56:27 +0000

    Preliminary JEI to deuterium extractor WIP

---
+++ src/main/java/net/joelinrome/plasmacraft/compat/DeuteriumExtractorRecipies.java
@@ -0,0 +1,60 @@
+package net.joelinrome.plasmacraft.compat;
+
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.joelinrome.plasmacraft.PlasmaCraft;
+import net.joelinrome.plasmacraft.block.ModBlocks;
+import net.joelinrome.plasmacraft.repice.DeuteriumExtractorRecipe;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+public class DeuteriumExtractorRecipies implements IRecipeCategory<DeuteriumExtractorRecipe> {
+    public static final ResourceLocation UID = new ResourceLocation(PlasmaCraft.MOD_ID, "deuterium_extracting");
+    public static final ResourceLocation TEXTURE = new ResourceLocation(PlasmaCraft.MOD_ID,
+             "textures/gui/deuterium_exractor_gui.png");
+
+    public static final RecipeType<DeuteriumExtractorRecipe> DEUTERIUM_EXTRACTOR_TYPE =
+            new RecipeType<>(UID, DeuteriumExtractorRecipe.class);
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public DeuteriumExtractorRecipies(IGuiHelper helper) {
+        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 85);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.DEUTERIUM_EXTRACTOR_BLOCK.get()));
+    }
+
+    @Override
+    public RecipeType<DeuteriumExtractorRecipe> getRecipeType() {
+        return DEUTERIUM_EXTRACTOR_TYPE;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.literal("Deuterium Extractor");
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return this.background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return this.icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder iRecipeLayoutBuilder, DeuteriumExtractorRecipe deuteriumExtractorRecipe, IFocusGroup iFocusGroup) {
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.INPUT, 80, 11).addIngredients(deuteriumExtractorRecipe.getIngredients().get(0));
+
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.OUTPUT, 80, 59).addItemStack(deuteriumExtractorRecipe.getResultItem(null));
+    }
+}
